b0cdf538ca4ea86289b60c339635008f6311996e
Vishal Verma <vermavishal891@gmail.com>
2023-03-23 01:39:48 +0530

    4th

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ let BACKEND_SERVER = null;
 if (process.env.REACT_APP_BACKEND_SERVER) {
   BACKEND_SERVER = process.env.REACT_APP_BACKEND_SERVER;
 } else {
-  BACKEND_SERVER = "http://localhost:5000/api/";
+  BACKEND_SERVER = "https://king-prawn-app-u6akm.ondigitalocean.app/api/";
 }
 
 const config = {
